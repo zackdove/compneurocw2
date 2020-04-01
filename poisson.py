2f5248993f6d73c2d904ab6fa17fd79d34566a1c
zackdove <zdove@live.co.uk>
2020-04-01 17:37:45 +0100

    rho done

--- poisson.py
+++ poisson.py
@@ -19,25 +19,6 @@ def get_spike_train(rate,big_t,tau_ref):
         t+=tau_ref+rnd.expovariate(exp_rate)
     return spike_train
 
-# calculate fano factor of spike count
-def get_fano_factor1(spike_train, big_t, window_size):
-    spike_count_list = []
-    for i in np.arange(0, big_t, window_size*ms):
-        start = i
-        end = i+window_size*ms
-        spikes_for_interval = 0
-        for spike in spike_train:
-            if spike > start and spike < end:
-                spikes_for_interval+=1
-        spike_count_list.append(spikes_for_interval)
-        # print("start =",start," end=",end," spikes=",spikes_for_interval)
-    # print(spike_count_list)
-    mean = np.mean(spike_count_list)
-    # print("mean = ", mean)
-    var = np.var(spike_count_list)
-    fano = var/mean
-    print("fano=",fano)
-    return fano
 
 # calculate fano factor of spike count
 def get_fano_factor(spike_train, big_t, window_size):
@@ -55,7 +36,7 @@ def get_fano_factor(spike_train, big_t, window_size):
     # print("mean = ", mean)
     var = np.var(spike_count_list)
     fano = var/mean
-    print("fano=",fano)
+    print("fano window size",window_size,"=",fano)
     return fano
 
 # calculate coefficient of variance of inter spike interval
@@ -73,6 +54,10 @@ def get_cov(spike_train):
     print ("cov=",cov)
     return cov
 
+def load_data(filename,T):
+    data_array = [T(line.strip()) for line in open(filename, 'r')]
+    return data_array
+
 # # variables:
 # rate=10.0 *Hz
 # # refractory period
@@ -88,12 +73,12 @@ def get_cov(spike_train):
 
 # 1000s, 35hz, no refractory
 big_t = 1000*sec
-rate = 35*Hz
+rate = 100*Hz
 tau_ref = 0*ms
 spike_train = get_spike_train(rate, big_t, tau_ref)
 print("1000s, 35hz, no refractory")
 # fano factor 10ms windows
-get_fano_factor(spike_train, big_t, 10)
+get_fano_factor(spike_train, big_t, 100)
 # fano factor 50ms windows
 get_fano_factor(spike_train, big_t, 50)
 # fano factor 100ms windows
@@ -115,3 +100,31 @@ get_fano_factor(spike_train, big_t, 50)
 get_fano_factor(spike_train, big_t, 100)
 # cov of interspike interval 1000s, 35Hz, no refractory
 get_cov(spike_train)
+
+
+def vec_to_train(vec, f):
+    f = 500*Hz
+    p = 1/f
+    # print(len(vec), t*f)
+    train = []
+    for index, s in enumerate(vec):
+        if (s):
+            train.append(index*p)
+    return train
+
+rho_vec=load_data("rho.dat",int)
+rho_train = vec_to_train(rho_vec, 500*Hz)
+print("rho:")
+# fano factor 10ms windows
+get_fano_factor(rho_train, 20*60*sec, 100)
+# fano factor 50ms windows
+get_fano_factor(rho_train, 20*60*sec, 50)
+# fano factor 100ms windows
+get_fano_factor(rho_train, 20*60*sec, 100)
+# cov of interspike interval 1000s, 35Hz, no refractory
+get_cov(spike_train)
+
+# stimulus=load_data("stim.dat",float)
+#
+# print(len(stimulus))
+# print(stimulus[0:5])
